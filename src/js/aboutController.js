81f088a6840fe16322f86eadb8326027adba633d
Anthony Campos <Anthonyck89@hotmail.com>
2023-04-12 09:19:55 -0700

    linkes checked

--- src/js/aboutController.js
+++ src/js/aboutController.js
@@ -1,8 +1,20 @@
 import memoriesView from './views/memoriesView.js';
-import { MEMORIES_COUNT } from './config.js';
-import { revealSections, lazyLoadImgBySection } from './helper.js';
+import { MEMORIES_COUNT, MOBILE_NAV_MEDIA_QUERY } from './config.js';
+import {
+  revealSections,
+  lazyLoadImgBySection,
+  mediaQueryMenu,
+} from './helper.js';
 
 const init = async function () {
+  // determine if mobile nav is needed
+  document.addEventListener('DOMContentLoaded', function () {
+    // check initial page load
+    mediaQueryMenu(MOBILE_NAV_MEDIA_QUERY);
+    // dynamically check when page changes
+    MOBILE_NAV_MEDIA_QUERY.addEventListener('change', mediaQueryMenu);
+  });
+
   const data = {
     memoryCount: MEMORIES_COUNT,
   };
@@ -13,10 +25,12 @@ const init = async function () {
   // get sections that will have a slide in effect applied to them
   const allSections = document.querySelectorAll('.section-effect');
 
-  // apply effect
-  revealSections(allSections);
-  // lazy load imgs by section
-  lazyLoadImgBySection(allSections);
+  if (allSections) {
+    // apply effect
+    revealSections(allSections);
+    // lazy load imgs by section
+    lazyLoadImgBySection(allSections);
+  }
 };
 
 init();
--- src/js/faqController.js
+++ src/js/faqController.js
@@ -1,4 +1,5 @@
 import Accordion from './component/accordion.js';
+
 import {
   supplyFaqs,
   activitiesFaqs,
