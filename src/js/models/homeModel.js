661995134528cb649079110cc0627de8f1c08902
Anthony Campos <Anthonyck89@hotmail.com>
2023-04-12 18:28:52 -0700

    progress

--- src/js/models/homeModel.js
+++ src/js/models/homeModel.js
@@ -1,3 +1,8 @@
+import camper1 from '../../img/home/testimonials/camper-1.jpg';
+import camper2 from '../../img/home/testimonials/camper-2.jpg';
+import camper3 from '../../img/home/testimonials/camper-3.jpg';
+import camper4 from '../../img/home/testimonials/camper-4.jpg';
+
 /** CAMPER testimonial Data */
 const campers = [
   {
@@ -7,27 +12,27 @@ const campers = [
 people I've met. The weekend is about
 relaxing and enjoying nature, surrounded by good people. You
 just might meet your soulmate amongst them.`,
-    camperImg: 'camper-1.jpg',
+    camperImg: camper1,
   },
 
   {
     camperName: 'Brian Duff',
     camperRole: 'Long Time Camper',
     camperText: `I can truly disconnect from the world and connect with nature. I've been a camper for years and each trip is filled with new adventures and memories with old and new friends. The hosts truly create a community of inclusivity and joy.`,
-    camperImg: 'camper-2.jpg',
+    camperImg: camper2,
   },
 
   {
     camperName: 'Ronnie Kwong',
     camperRole: 'Long Time Camper',
     camperText: `I've been attending this camping trip for six years now, and I can honestly say it's the highlight of my year. The camaraderie and outdoor activities are truly second to none, and the beautiful location only adds to the experience.`,
-    camperImg: 'camper-3.jpg',
+    camperImg: camper3,
   },
   {
     camperName: 'Junior Castro',
     camperRole: '2nd Year Camper',
     camperText: `Camp Sus is the perfect getaway for my family. We've attended twice, and each time we've had the most amazing experiences. My son, in particular, loves it here, and he always looks forward to it every year. Thank you, Camp Sus!`,
-    camperImg: 'camper-4.jpg',
+    camperImg: camper4,
   },
 ];
 
